d59acb893d940c5b5135da13dfbc4223199ddc7f
Latex-Opony <knowicki@latexopony.eu>
2025-04-28 11:31:26 +0200

    Initial commit – pełen projekt Godot

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 
-@export var speed := 200.0
-@export var gravity := 500.0
-@export var jump_velocity := -300.0
+@export var speed       : float = 200.0
+@export var gravity     : float = 800.0
+@export var jump_force  : float = -350.0
 
-func _physics_process(delta):
-	# Dodajemy grawitację
-	if not is_on_floor():
-		velocity.y += gravity * delta
+var can_wall_jump : bool = true
 
-	# Ruch w poziomie
+func _physics_process(delta: float) -> void:
+	# 1) Sterowanie poziome
+	var dir := 0.0
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-	else:
-		velocity.x = 0
+		dir += 1
+	if Input.is_action_pressed("ui_left"):
+		dir -= 1
+	velocity.x = dir * speed
 
-	# Skok
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+	# 2) Reset wall-jump gdy na ziemi
+	if is_on_floor():
+		can_wall_jump = true
 
-	# Przesuwamy węzeł i aktualizujemy velocity
-	move_and_slide()
+	# 3) Skok
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_force
+		elif is_on_wall() and can_wall_jump:
+			velocity.y = jump_force
+			# odbij w poziomie od ściany
+			var n = get_last_slide_collision().normal
+			velocity.x = -n.x * speed
+			can_wall_jump = false
+
+	# 4) Grawitacja
+	velocity.y += gravity * delta
+
+	# 5) Przesunięcie
+	move_and_slide()  # korzysta z tego.velocity
+
+	# 6) Kamera podąża pionowo
+	$Camera2D.position.y = global_position.y
---
+++ Scenes/main.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+const PlatformScene = preload("res://Scenes/Platform.tscn")
+
+@export var spawn_buffer_y := 300
+@export var min_gap := 150
+@export var max_gap := 250
+
+var platforms : Array[StaticBody2D] = []
+var next_spawn_y : float
+
+@onready var cam    : Camera2D         = $Player/Camera2D
+@onready var player : CharacterBody2D  = $Player
+
+func _ready() -> void:
+	randomize()
+	next_spawn_y = player.global_position.y
+	# pierwsza platforma dokładnie tam, gdzie stoi gracz
+	_spawn_platform(next_spawn_y)
+	# kilka kolejnych poniżej
+	for i in 5:
+		next_spawn_y -= randi_range(min_gap, max_gap)
+		_spawn_platform(next_spawn_y)
+
+func _process(_delta: float) -> void:
+	var py = player.global_position.y
+
+	# dorzucaj nowe tak długo, aż są w buforze nad graczem
+	while next_spawn_y > py - spawn_buffer_y:
+		next_spawn_y -= randi_range(min_gap, max_gap)
+		_spawn_platform(next_spawn_y)
+
+	# usuwaj te, co wypadły poniżej ekranu
+	for p in platforms.duplicate():
+		if p.global_position.y > py + spawn_buffer_y:
+			platforms.erase(p)
+			p.queue_free()
+
+func _spawn_platform(y_pos: float) -> void:
+	# 1) pobierz rozmiar widocznego ekranu w pikselach
+	var screen_px = get_viewport().get_visible_rect().size
+	# 2) oblicz połowę szerokości widoku w jednostkach świata, uwzględniając zoom
+	var half_w = screen_px.x * cam.zoom.x * 0.5
+	# 3) wyznacz granice X
+	var left  = cam.global_position.x - half_w
+	var right = cam.global_position.x + half_w
+	# 4) losuj X w tych granicach
+	var x_pos = randf_range(left, right)
+
+	# 5) instancjuj platformę i ustaw pozycję
+	var p = PlatformScene.instantiate()
+	p.global_position = Vector2(x_pos, y_pos)
+	add_child(p)
+	platforms.append(p)
